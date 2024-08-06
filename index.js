b5380afc462890e013695f0a42f3fca9db37c311
zakwatsonn <168912087+zakwatsonn@users.noreply.github.com>
2024-08-06 17:15:13 +0100

    completed the function to create the grid

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const container = document.querySelector('.container')
 
-
 function createGrid(sizeOfGrid) {
-   for (let i = 0; i < sizeOfGrid*sizeOfGrid; i++) {
-    let gridBoxes = document.createElement('div');
-    gridBoxes.classList.add('gridBoxes')
-    container.append(gridBoxes)
-} 
+   for (let i = 0; i < sizeOfGrid; i++) {
+     let rowBoxes
+     rowBoxes = document.createElement('div')
+     rowBoxes.classList.add('rowBoxes')
+     container.append(rowBoxes)
+     for (let j = 0; j < sizeOfGrid; j++) {
+        let colBoxes = document.createElement('div')
+        colBoxes.classList.add('colBoxes')
+        rowBoxes.append(colBoxes)
+     }
+    } 
 }
+
+
+//prompting to get the size of the grid
+createGrid(prompt('Give the width of the grid in squares.'))
+
 
